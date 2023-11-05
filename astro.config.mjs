b383667faae92953cef242cb6f54093edbb80bd2
Vladislav Guleaev <Vladislav.Guleaev@check24.de>
2023-11-05 11:03:09 +0100

    Add Vercel integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,21 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from '@astrojs/tailwind';
+
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://vguleaev-dev.vercel.app',
   integrations: [mdx(), sitemap(), tailwind()],
+  output: 'static',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+    speedInsights: {
+      enabled: true,
+    },
+  }),
 });
