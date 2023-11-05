2e20243bc6fad08ea1ba354515c26ce85019a9a0
Vladislav Guleaev <Vladislav.Guleaev@check24.de>
2023-11-05 12:11:04 +0100

    Add new favicon

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,9 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Vlad Guleaev';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Vladislav Guleaev';
+export const SITE_DESCRIPTION =
+  'Personal Blog by Vladislav Guleaev. I write about web development, programming, and other tech stuff.';
 export const TWITTER_LINK = 'https://twitter.com/vguleaev1';
 export const LINKEDIN_LINK = 'https://www.linkedin.com/in/vladislav-guleaev-15503914a';
 export const GITHUB_LINK = 'https://github.com/vguleaev';
